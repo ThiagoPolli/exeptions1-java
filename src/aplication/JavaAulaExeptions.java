11b26be7a66670d14898d0cd3b0558038aadb16e
ThiagoPolli <thiagopolli.palma@gmail.com>
2019-12-06 22:40:24 -0300

    Versao muito ruim

---
+++ src/aplication/JavaAulaExeptions.java
@@ -0,0 +1,53 @@
+
+package aplication;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+import modeloEntidade.Reserva;
+
+public class JavaAulaExeptions {
+
+
+    public static void main(String[] args) throws ParseException {
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        
+        System.out.print("Room Number ");
+        int number = sc.nextInt();
+        
+        System.out.print("Check-In Date (dd/MM/yyyy ): ");
+        Date checkIn = sdf.parse(sc.next());
+        
+         System.out.print("Check-Out Date (dd/MM/yyyy ): ");
+        Date checkOut = sdf.parse(sc.next());
+        
+        if (!checkOut.after(checkIn)){
+            
+            System.out.println("Data Invalida");
+            
+        }
+        else {
+            Reserva reserva = new Reserva(number, checkIn, checkOut);
+            System.out.println("Reserva "+reserva);
+            
+            System.out.println();
+            System.out.println("Entre com a data atualização ");
+            
+            System.out.print("Check-In Date (dd/MM/yyyy ): ");
+            checkIn = sdf.parse(sc.next());
+        
+            System.out.print("Check-Out Date (dd/MM/yyyy ): ");
+            checkOut = sdf.parse(sc.next());
+            
+            reserva.updateDates(checkIn, checkOut);
+            System.out.println("Reserva "+reserva);
+        }
+        
+        sc.close();
+
+
+    }
+    
+}
---
+++ src/modeloEntidade/Reserva.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modeloEntidade;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+import sun.java2d.pipe.SpanShapeRenderer;
+
+/**
+ *
+ * @author THIAGO
+ */
+public class Reserva {
+    
+    private Integer roomnamber;
+    private Date checkIn;
+    private Date checkOut;
+    
+    private static SimpleDateFormat sdf  = new SimpleDateFormat("dd/MM/yyyy");
+
+    public Reserva(Integer roomnamber, Date checkIn, Date checkOut) {
+        this.roomnamber = roomnamber;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public Integer getRoomnamber() {
+        return roomnamber;
+    }
+
+    public void setRoomnamber(Integer roomnamber) {
+        this.roomnamber = roomnamber;
+    }
+
+    public Date getCheckIn() {
+        return checkIn;
+    }
+
+
+    public Date getCheckOut() {
+        return checkOut;
+    }
+
+    public long duration(){
+        long diff =  checkOut.getTime()-checkIn.getTime();
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+        
+    }
+    
+    public void updateDates(Date checkIn, Date checkOut ){
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+    @Override
+    public String toString(){
+        return "Room " + roomnamber + ", check-In " +sdf.format(checkIn) + ", check-Out  " + sdf.format(checkOut)+" , "+  duration() + " Night";
+    }
+    
+}
